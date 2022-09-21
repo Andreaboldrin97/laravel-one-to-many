1cc25900d6c4d6ac28ac9d38b427e7549417e185
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-09-21 16:47:41 +0200

    add where on postController in index

--- app/Http/Controllers/admin/PostController.php
+++ app/Http/Controllers/admin/PostController.php
@@ -23,7 +23,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::all();
+        $posts = Post::WHERE('user_id', Auth::id())->get();
         return view('admin.post.index', compact('posts'));
     }
 
